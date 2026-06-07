cb8cacb0f90a9aac171908aa39cd0ab8fc3d098f
Jeryd <jerydleuck@gmail.com>
2026-06-06 20:03:04 -1000

    Configure installers and Linux packages to bundle the desktop launcher and icon

--- packaging/deb/build-debian-package.sh
+++ packaging/deb/build-debian-package.sh
@@ -6,6 +6,8 @@ echo "Building Debian package..."
 
 # Create staging directory structure
 mkdir -p debian/usr/bin
+mkdir -p debian/usr/share/applications
+mkdir -p debian/usr/share/pixmaps
 mkdir -p ../../dist/packages
 
 # Locate and copy binary
@@ -22,8 +24,15 @@ fi
 
 chmod 755 debian/usr/bin/rstart
 
+# Copy desktop file and icon
+cp ../desktop/rstart.desktop debian/usr/share/applications/rstart.desktop
+cp ../../assets/brand/app_icon.png debian/usr/share/pixmaps/rstart.png
+chmod 644 debian/usr/share/applications/rstart.desktop
+chmod 644 debian/usr/share/pixmaps/rstart.png
+
 # Run dpkg-deb to build the package
 dpkg-deb --build debian ../../dist/packages/rstart.deb
 
-# Clean up staging binary
+# Clean up staging files
 rm -f debian/usr/bin/rstart
+rm -rf debian/usr/share
--- packaging/rpm/rstart.spec
+++ packaging/rpm/rstart.spec
@@ -18,7 +18,13 @@ cargo build --release --locked
 %install
 rm -rf $RPM_BUILD_ROOT
 install -d $RPM_BUILD_ROOT/%{_bindir}
+install -d $RPM_BUILD_ROOT/%{_datadir}/applications
+install -d $RPM_BUILD_ROOT/%{_datadir}/pixmaps
 install -m 755 target/release/rstart $RPM_BUILD_ROOT/%{_bindir}/rstart
+install -m 644 packaging/desktop/rstart.desktop $RPM_BUILD_ROOT/%{_datadir}/applications/rstart.desktop
+install -m 644 assets/brand/app_icon.png $RPM_BUILD_ROOT/%{_datadir}/pixmaps/rstart.png
 
 %files
 %{_bindir}/rstart
+%{_datadir}/applications/rstart.desktop
+%{_datadir}/pixmaps/rstart.png
