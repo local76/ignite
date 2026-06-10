aefeced5fc306862739297af99d8a166ba4ca4e9
Jeryd <jerydleuck@gmail.com>
2026-06-10 03:49:37 -1000

    modernize to 4.2 paths in app-ignite

--- src/win32.rs
+++ src/win32.rs
@@ -1,13 +1,9 @@
-#[allow(unused_imports)]
-pub use library::clipboard::copy_text_to_clipboard;
-pub use library::window::{
+pub use library::toolkit::clipboard::copy_text_to_clipboard;
+pub use library::apps::window::{
     query_cursor_pos, get_window_rect, set_window_pos, relaunch_in_conhost_if_needed,
+    hide_console_at_startup, show_console_window,
 };
-#[allow(unused_imports)]
-pub use library::sys_info::{
-    get_console_window_dpi, get_system_screen_resolution, query_bios_info,
-    query_dark_mode, query_os_version, query_power_status, query_shell_and_terminal,
+pub use library::toolkit::sys_info::{
+    query_dark_mode, query_os_version, query_power_status,
     get_dwm_accent_color, GlyphMap,
 };
-
-pub use library::{hide_console_at_startup, show_console_window};
